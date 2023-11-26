e765a9a9d2592afa8f13d5405c9403643708232b
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-26 14:19:12 +0100

    Update script.js

--- sitez/script.js
+++ sitez/script.js
@@ -1,21 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const backgroundCode = document.createElement('div');
-    backgroundCode.classList.add('background-code');
-    document.body.appendChild(backgroundCode);
 
-    const pre = document.createElement('pre');
-    pre.textContent = `# RunBook.ai
-
-def automate_runbook():
-    # Write your automation logic here
-    print("Automating runbook with AI assistance")
-
-automate_runbook()`;
-
-    backgroundCode.appendChild(pre);
-});
-
- const owner = "kodesam";
+        const owner = "kodesam";
         const repo = "collection-i-runbooks";
         const folders = ["python", "ansible", "linux"];
 
